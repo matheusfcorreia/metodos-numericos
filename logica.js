811968f4e01c1da9e14f5f3b03367e8aa25b0c42
Matheus <matheus.freitas@softcenter.com.br>
2020-10-25 09:44:20 -0300

    Gauss Elimination

--- logica.js
+++ logica.js
@@ -1,3 +1,10 @@
+const { create, all } = require('mathjs');
+const math = create(all);
+
+math.config({
+  number: 'Fraction'
+})
+
 
 const printMatrix = (matrix) => {
   let print = '';
@@ -6,7 +13,7 @@ const printMatrix = (matrix) => {
     let line = '';
     for (const elem of row) {
       elem.toString().length > 1 ? line += `${elem}  ` :
-      line += `${elem}   `
+        line += `${elem}   `
     }
     print += `${line} \n`
   }
@@ -46,4 +53,117 @@ const getSystem = (systems) => {
   return systemsArray;
 }
 
-const system = ['x + 2y + 3z = 1', '2x + y - z = -2', '9x - 2y + z = 2'];
+// const system = ['x + 2y + 3z = 1', '2x + y - z = -2', '9x - 2y + z = 2'];
+
+
+function increasedMatrix(matrix, vector) {
+  const increasedMatrix = []
+  matrix.map((element) => increasedMatrix.push(element));
+
+  for (let i = 0; i < increasedMatrix.length; i++) {
+    increasedMatrix[i].push(vector[i])
+  }
+
+  return increasedMatrix;
+}
+
+function system(a, b){
+  expressoes = []
+  a.forEach((el, index) => {
+      expr = ''
+      for (i = index; i < el.length - 1; i++) {
+          expr += `+(${el[i]}) X${i + 1} `
+      }
+      expr += ` = (${b[index]})`
+      expressoes.push(expr)
+  })
+  return expressoes
+}
+
+function gaussElimination(matrix) {
+  memory = []
+  increasedMatrix = matrix
+
+  memory.push({ acao: 'Inicio da matriz A', 'matriz': increasedMatrix.toString() })
+
+  columnPivo = 0
+  linePivo = 0
+
+  for (i = 0; i < increasedMatrix.length - 1; i++) {
+    if (increasedMatrix[linePivo][columnPivo] == '0') {
+
+      biggerColumnsElem = 0
+      biggerRowElem = 0
+
+      for (k = linePivo + 1; k < increasedMatrix.length; k++) {
+        element = increasedMatrix[k][columnPivo];
+        if (element >= biggerColumnsElem) {
+          biggerColumnsElem = element
+          biggerRowElem = k
+        }
+      }
+
+      if (biggerColumnsElem == '0') continue
+
+      auxLine = increasedMatrix[linePivo]
+      increasedMatrix[linePivo] = increasedMatrix[biggerRowElem]
+      increasedMatrix[biggerRowElem] = auxLine
+      memory.push({ acao: 'Pivotamento de linha', 'matrix': increasedMatrix.toString() })
+    }
+
+    pivo = increasedMatrix[linePivo][columnPivo]
+    memory.push({ acao: 'Pivô', 'pivo': pivo })
+
+    for (j = linePivo + 1; j < increasedMatrix.length; j++) {
+      line = j
+
+      lineMultiplier = math.divide(math.evaluate(increasedMatrix[line][columnPivo]), math.evaluate(pivo))
+      memory.push({ acao: `Multiplicador da linha ${line}`, 'matriz': lineMultiplier.toString() })
+
+      increasedMatrix[line].forEach((lineElement, elemIndex) => {
+        multiplication = math.multiply(lineMultiplier, increasedMatrix[linePivo][elemIndex])
+        resultado = math.format(math.subtract(lineElement, multiplication))
+        increasedMatrix[line][elemIndex] = resultado.split('/')[1] == '1' ? resultado.split('/')[0] : resultado
+        memory.push({ acao: `Operação com o elemento ${elemIndex} da linha ${line}`, 'matriz': increasedMatrix.toString() })
+      });
+    }
+
+    linePivo += 1
+    columnPivo += 1
+  }
+
+  increasedMatrixAux = increasedMatrix
+  newVector = []
+  for (i = 0; i < increasedMatrixAux.length; i++) {
+    newVector.push(math.fraction(increasedMatrixAux[i].pop()))
+    for (j = 0; j < increasedMatrixAux[i].length; j++) {
+      increasedMatrixAux[i][j] = math.fraction(increasedMatrixAux[i][j])
+    }
+  }
+
+  expressions = system(increasedMatrixAux, newVector)
+
+  solutions = math.usolve(increasedMatrixAux, newVector)
+
+  solutions.forEach((element, index) => {
+    formatted = math.format(element).slice(1, -1)
+    solutions[index] = formatted.split('/')[1] == '1' ? formatted.split('/')[0] : formatted
+  })
+  memory.push({ acao: `Matriz Aumentada`, matriz: increasedMatrixAux.toString() })
+  memory.push({ acao: `Vetor b`, matriz: newVector.toString() })
+  memory.push({ acao: `Sistema`, memory: expressions })
+  memory.push({ acao: `Solução`, memory: solutions })
+
+  return {
+    solutions,
+    expressions,
+    memory
+  }
+
+}
+
+const matrix = [[2, 1, -1, -3], [1, 2, 1, 3], [1, 1, 1, 2]];
+
+const res = gaussElimination(matrix);
+
+console.log(res);
